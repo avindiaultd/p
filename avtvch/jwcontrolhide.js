40e88c9dbde62c1f316a92b54b52c077e448cea6
avindiaultd <168712840+avindiaultd@users.noreply.github.com>
2024-05-11 13:17:28 +0530

    Update jwcontrolhide.js

--- avtvch/jwcontrolhide.js
+++ avtvch/jwcontrolhide.js
@@ -17,7 +17,7 @@ var jwDefaults = {
   "displaytitle": false,
   "height": 260,
   "key": "Ue3nwln8gRYs0JEtq3V4ptnuJKH3YaexK186vqp4pidNB9HBtz7gfvq/LXqcvuI5",
-  "mute": true,
+  "mute": false,
   "ph": 1,
   "pid": "IDzF9Zmk",
   "playbackRateControls": false,
