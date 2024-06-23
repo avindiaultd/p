4d1b7deb339466160ef97994941104bb7f74236f
AV INDIA ULTD <168712840+avindiaultd@users.noreply.github.com>
2024-06-23 11:20:22 +0530

    Create firebase-config.js and private

---
+++ web/firebase-config.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyDHL4CSEjJBdNUI4IozRI-7Suqd4UqUoDI",
+  authDomain: "avindia-db2bd.firebaseapp.com",
+  databaseURL: "https://avindia-db2bd-default-rtdb.firebaseio.com/",
+  projectId: "avindia-db2bd",
+  storageBucket: "avindia-db2bd.appspot.com",
+  messagingSenderId: "970872368951"
+};
